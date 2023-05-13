6b89c5fce456bac8bd149739fbe0e45067a3633e
hayashi.yuhki <hayashi.yuhki@gmail.com>
2023-05-13 13:30:25 +0900

    react-query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
 import Router from './Router'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import './App.css'
 import { ExampleProvider, UserProvider } from './context/index'
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 
 function App() {
 
@@ -10,7 +17,10 @@ function App() {
     <>
       <UserProvider>
         <ExampleProvider>
-          <Router />
+          <QueryClientProvider client={queryClient}>
+            <Router />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
         </ExampleProvider>
       </UserProvider>
     </>
--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,30 +1,48 @@
-import {useContext, useEffect} from 'react'
-import {ExampleContext, UserContext} from '../context'
+import { useContext } from 'react'
+import { ExampleContext } from '../context'
 import { Link } from "react-router-dom";
 import axios from 'axios';
-
-export default function About () {
-
-  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // 環境変数から API のベース URL を取得
-
-  useEffect(() => {
-    axios.get(`${API_BASE_URL}/api/v1/example`).then((res) => {
-      console.log(res.data)
-    })
-  }, [])
+import {
+  useQuery,
+} from '@tanstack/react-query'
 
 
-  const { counter , incrementCounter} = useContext(ExampleContext);
+export default function About() {
 
-  const onClickHandler = ()=> {
-    incrementCounter()
-  }
 
+  const { data, isSuccess, isLoading, error } = useQuery({
+     queryKey:['example'],
+    queryFn: () => axios(`${import.meta.env.VITE_API_BASE_URL}/api/v1/example`).then(res => res.data),
+    staleTime: 300000, // 5 分間
+    onError: (error) => {
+      console.log(error)
+    }
+  })
+
+
+  const { counter, incrementCounter } = useContext(ExampleContext);
+
+  const onClickHandler = () => incrementCounter()
+
+  if (isLoading) return 'Loading...'
+
+  if(error) return 'An error has occurred: ' + error
   return (
     <div>
       <h1>from context: {counter}</h1>
+      <div>{isSuccess && data.map((item: any, index: number) => {
+        return (
+          <div key={index}>
+            <h1>{item.name}</h1>
+            <p>{item.email}</p>
+          </div>
+        )
+      })
+
+      }
+      </div>
       <button onClick={onClickHandler} className={`bg-black h-8 w-20 rounded text-white`}>Click</button>
-       <Link to="/">Home</Link>
+      <Link to="/">Home</Link>
     </div>
   );
 }
