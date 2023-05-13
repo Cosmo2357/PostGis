ba2f7b8d68cacf73fa9c08b03174cdccc54e9c2f
hayashi.yuhki <hayashi.yuhki@gmail.com>
2023-05-13 15:01:56 +0900

    added react-form

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form';
 import { ExampleContext } from '../context'
 import { Link } from "react-router-dom";
 import axios from 'axios';
@@ -8,13 +9,29 @@ import {
 
 export default function About() {
 
+  type Inputs = {
+    example: string;
+    exampleRequired: string;
+  };
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors }
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log('onSubmit:', data);
+  console.log('watch:', watch('example')); // watchは引数に渡した名前の入力値を監視する
+  console.log('exampleRequired:', watch('exampleRequired'));
+
+
   const { data, isSuccess, isLoading, error } = useQuery({
-     queryKey:['example'],
+    queryKey: ['example'],
     queryFn: () => axios(`${import.meta.env.VITE_API_BASE_URL}/api/v1/example`).then(res => res.data),
     staleTime: 300000, // 5 分間
     onError: (error) => {
       console.log(error)
-    }})
+    }
+  })
 
   const { counter, incrementCounter } = useContext(ExampleContext);
 
@@ -22,7 +39,7 @@ export default function About() {
 
   if (isLoading) return 'Loading...'
 
-  if(error) return 'An error has occurred: ' + error
+  if (error) return 'An error has occurred: ' + error
   return (
     <div>
       <h1>from context: {counter}</h1>
@@ -31,6 +48,7 @@ export default function About() {
           <div key={index}>
             <h1>{item.name}</h1>
             <p>{item.email}</p>
+
           </div>
         )
       })
@@ -39,6 +57,16 @@ export default function About() {
       </div>
       <button onClick={onClickHandler} className={`bg-black h-8 w-20 rounded text-white`}>Click</button>
       <Link to="/">Home</Link>
+      <div >
+        <form className="flex p-2 flex-col" onSubmit={handleSubmit(onSubmit)}>
+          <input className="border p-2 my-2 rounded" defaultValue="test" {...register('example')} />
+          <input className="border p-2  my-2 rounded " {...register('exampleRequired', { required: true })} />
+          {errors.exampleRequired && (
+            <span style={{ color: 'red' }}>This field is required</span>
+          )}
+          <button className="btn bg-white my-2  h-11 py-1 px-4 rounded border" type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
